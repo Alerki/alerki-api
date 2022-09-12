d4830a4d78692366067f405e5458c4d33e4ae3f0
Petryk Oleksandr <sachapos50@gmail.com>
2022-09-12 23:46:20 +0300

    feat: set env variable decorator and tests

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { AuthModule } from '@Module/auth/auth.module';
+import { UserModule } from '@Module/user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UserModule],
 })
 export class AppModule {}
