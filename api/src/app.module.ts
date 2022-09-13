5bbc50b680768cf6c537f03908c724c5a3dde621
Petryk Oleksandr <sachapos50@gmail.com>
2022-09-14 00:25:32 +0300

    feat: save

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '@Module/auth/auth.module';
 import { UserModule } from '@Module/user/user.module';
 
+
 @Module({
   imports: [AuthModule, UserModule],
 })
