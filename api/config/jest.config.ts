c1abdc7daf456b7e78572d817436c58ba291d3a6
Petryk Oleksandr <sachapos50@gmail.com>
2022-09-23 02:10:23 +0300

    fix: paths

--- api/config/jest.config.ts
+++ api/config/jest.config.ts
@@ -2,7 +2,7 @@ export default {
   moduleNameMapper: {
     '^@Config/(.*)$': '<rootDir>/config/$1',
     '^@Shared/(.*)$': '<rootDir>/src/shared/$1',
-    '^@Module/(.*)$': '<rootDir>/src/modules/$1',
+    '^@Module/(.*)$': '<rootDir>/src/module/$1',
   },
   testEnvironment: 'node',
   testMatch: ['**/?(*.)+(spec|test|e2e-spec).[jt]s?(x)'],
