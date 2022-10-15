21c6fb1e6f85de46bc3c44aeb285d1620f94bba1
Petryk Oleksandr <sachapos50@gmail.com>
2022-10-15 17:15:39 +0300

    feat: jest config

--- api/jest.e2e.config.ts
+++ api/jest.e2e.config.ts
@@ -5,14 +5,15 @@ import jestConfig from './config/jest.config';
 const config: Config = {
   ...jestConfig,
   roots: ['./test'],
-  // coverageThreshold: {
-  //   global: {
-  //     branches: 80,
-  //     functions: 80,
-  //     lines: 80,
-  //     statements: 80,
-  //   },
-  // },
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  testTimeout: 30000,
 };
 
 export default config;
