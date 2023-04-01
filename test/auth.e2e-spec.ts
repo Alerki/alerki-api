f1153f39bffe257a14e747ed162148d2118f93e9
Oleksandr Petryk <sachapos50@gmail.com>
2023-04-01 14:12:08 +0300

    chore: save changes

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -1,20 +1,33 @@
-import type { INestApplication } from '@nestjs/common';
-import { ValidationPipe } from '@nestjs/common';
-import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { PrismaClient } from '@prisma/client';
 import * as cookieParser from 'cookie-parser';
 import type { Application } from 'express';
 import * as request from 'supertest';
 
 import { AppModule } from '../src/app.module';
+import { AuthModule } from '../src/auth/auth.module';
+import { AuthService } from '../src/auth/services/auth.service';
+import { AuthModuleService } from '../src/auth/services/auth-module.service';
+import { UserActions } from './utils/actions.util';
+import { clearDatabaseUtil } from './utils/clear-database.util';
 
 describe('Auth module testing', () => {
   let app: Application;
   let application: INestApplication;
+  let prisma: PrismaClient;
+
+  let authModuleService: AuthModuleService;
+  let authService: AuthService;
 
   beforeAll(async () => {
-    const moduleFixtures: TestingModule = await Test.createTestingModule({
+    const moduleFixtures = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
+
+    authModuleService =
+      moduleFixtures.get<AuthModuleService>(AuthModuleService);
+    authService = moduleFixtures.get<AuthService>(AuthService);
 
     application = await moduleFixtures
       .createNestApplication()
@@ -24,29 +37,42 @@ describe('Auth module testing', () => {
           whitelist: true,
           transform: true,
         }),
-      );
+      )
+      .init();
 
     app = application.getHttpServer();
+
+    prisma = new PrismaClient();
+  });
+
+  beforeEach(async () => {
+    await clearDatabaseUtil(prisma);
   });
 
   afterAll(async () => {
     await application.close();
   });
 
-  describe('Regular script', () => {
-    const user = {
-      email: 'new-user@gmail.com',
-      username: 'new_user',
-      password: '12345678',
-    };
+  describe('Test all endpoints', () => {
+    it('should register user', async () => {
+      const user = new UserActions(app);
 
-    it('register new user', async () => {
+      const usersBefore = await prisma.user.findMany({});
+
+      expect(usersBefore).toHaveLength(0);
+
       const r = await request(app)
         .post('/auth/register')
-        .send(user)
-        .expect(200);
+        .send(user.credentials)
+        .expect(201);
 
-      expect(r.body.accessToken).toBeDefined();
+      const usersAfter = await prisma.user.findMany({});
+
+      expect(usersAfter).toHaveLength(1);
+
+      expect(usersAfter[0].email).toBe(user.email);
+      expect(usersAfter[0].username).toBe(user.username);
+      expect(usersAfter[0].password).toBeDefined();
     });
   });
 });
---
+++ test/utils/clear-database.util.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from '@prisma/client';
+
+export const clearDatabaseUtil = async (prisma: PrismaClient) => {
+  await prisma.session.deleteMany({});
+  await prisma.userEmail.deleteMany({});
+  await prisma.userPhoneNumber.deleteMany({});
+  await prisma.clientProfile.deleteMany({});
+  await prisma.clientProfile.deleteMany({});
+  await prisma.user.deleteMany({});
+};
